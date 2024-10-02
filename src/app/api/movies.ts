3d0c603c106291afa30d8b05f995255b255256c0
BohdanKonovalov21 <konovalovbogdan21@gmail.com>
2024-10-02 14:39:37 +0300

    changed api url

--- src/app/api/movies.ts
+++ src/app/api/movies.ts
@@ -1,5 +1,6 @@
 export const getCollections = async () => {
-  const apiUrl = 'http://localhost:5001/api/collections';
+//   const apiUrl = 'http://localhost:5001/api/collections';
+  const apiUrl = 'https://screenify.one/api/collections';
 
   try {
     const response = await fetch(apiUrl);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,16 @@ export default async function Home() {
     (collection: any) => collection.slug === 'movies'
   );
 
+  console.log(collectionsData.collections[0].collectionFictions);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        {movies?.collectionFictions?.map((movie: Movie, index: number) => (
-          <MovieComponent movie={movie.fiction.movie} key={index} />
-        ))}
+        {collectionsData.collections[0].collectionFictions.map(
+          (movie: Movie, index: number) => (
+            <MovieComponent movie={movie.fiction.movie} key={index} />
+          )
+        )}
       </main>
     </div>
   );
